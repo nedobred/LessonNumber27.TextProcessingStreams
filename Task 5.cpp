3e2570124aedbe98e0331df9a99d549858ccd17e
ned66 <ned66@DENIS>
2025-02-25 16:17:47 +0500

    Task 5

--- Task 5.cpp
+++ Task 5.cpp
@@ -1,14 +1,14 @@
 // Дополните свою библиотеку функцией
 // реализациями нижеследующих функций:
-// char *Uppercase(char *strl); - 
+// char *Lowercase(char *strl); - 
 // функция преобразует строку в нижний 
 // регистр 
 
 #include <iostream>
 
-char *Uppercase(char *strl)
+char *Lowercase(char *strl)
 {
-    int count{0};
+    int count{ 0 };
     for (int i = 0; i < strlen(strl); i++)
     {
         strl[i];
@@ -18,9 +18,9 @@ char *Uppercase(char *strl)
     for (int i = 0; i < strlen(strl); i++)
     {
         arr[i] = strl[i];
-        if (arr[i] > 90)
+        if (arr[i] < 90 && arr[i] > 40)
         {
-            arr[i] -= 32;
+            arr[i] += 32;
         }
     }
     for (int i = 0; i < strlen(strl); i++)
@@ -35,7 +35,7 @@ int main()
 {
     setlocale(LC_ALL, "");
     char ch[] = "Hello world";
-    
-    std::cout  << Uppercase(ch) << std::endl;
+
+    std::cout << Lowercase(ch) << std::endl;
     return 0;
 }
